fcbe15a702b0de514e79ac4138c5876acdfee321
rramiachraf <51409801+rramiachraf@users.noreply.github.com>
2023-09-12 15:09:26 +0100

    Add missing React imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,8 @@ module.exports = {
 	env: { browser: true, es2020: true },
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
 		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
 		'plugin:react/jsx-runtime',
 	],
@@ -15,12 +15,9 @@ module.exports = {
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true },
-		]
-	},
-	settings: {
-		react: {
-			version: 'detect'
-		}
+		],
+		"react/jsx-uses-vars": "error",
+		"react/jsx-uses-react": "error"
 	}
 }
 
